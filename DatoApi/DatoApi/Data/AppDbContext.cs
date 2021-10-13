e85c6699a169357ffe28ace305a361e11d485da2
MariaIreneRiveroBenavides <a2020113350@estudiantes.upsa.edu.bo>
2021-10-12 20:44:54 -0400

    AppDbContext creado

---
+++ DatoApi/DatoApi/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DatoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatoApi.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Dato> Datos { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+    }
+}
