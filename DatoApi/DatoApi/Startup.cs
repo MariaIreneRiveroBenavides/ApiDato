c9ff5fc2fc87ade365e80c46e4f4c692d1d8a960
MariaIreneRiveroBenavides <a2020113350@estudiantes.upsa.edu.bo>
2021-10-12 20:47:20 -0400

    IreneContext agregado a Startup.cs

--- DatoApi/DatoApi/Startup.cs
+++ DatoApi/DatoApi/Startup.cs
@@ -1,7 +1,9 @@
+using DatoApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,14 @@ namespace DatoApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DatoApi", Version = "v1" });
             });
+
+            services.AddDbContext<AppDbContext>
+                (
+                options => options.UseSqlServer
+                (
+                    Configuration.GetConnectionString("IreneContext")
+                    )
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
