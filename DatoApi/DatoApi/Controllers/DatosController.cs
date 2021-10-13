772ebab0dd993c5b33fe8e7e766cdd519501db45
MariaIreneRiveroBenavides <a2020113350@estudiantes.upsa.edu.bo>
2021-10-12 20:50:32 -0400

    Migrations implementado

---
+++ DatoApi/DatoApi/Controllers/DatosController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DatoApi.Data;
+using DatoApi.Models;
+
+namespace DatoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DatosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Datos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Dato>>> GetDatos()
+        {
+            return await _context.Datos.ToListAsync();
+        }
+
+        // GET: api/Datos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Dato>> GetDato(string id)
+        {
+            var dato = await _context.Datos.FindAsync(id);
+
+            if (dato == null)
+            {
+                return NotFound();
+            }
+
+            return dato;
+        }
+
+        // PUT: api/Datos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDato(string id, Dato dato)
+        {
+            if (id != dato.Nombre)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(dato).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DatoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Datos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Dato>> PostDato(Dato dato)
+        {
+            _context.Datos.Add(dato);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DatoExists(dato.Nombre))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetDato", new { id = dato.Nombre }, dato);
+        }
+
+        // DELETE: api/Datos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDato(string id)
+        {
+            var dato = await _context.Datos.FindAsync(id);
+            if (dato == null)
+            {
+                return NotFound();
+            }
+
+            _context.Datos.Remove(dato);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DatoExists(string id)
+        {
+            return _context.Datos.Any(e => e.Nombre == id);
+        }
+    }
+}
---
+++ DatoApi/DatoApi/Migrations/20211013004947_AddModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatoApi.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Datos",
+                columns: table => new
+                {
+                    Nombre = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pelicula = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cancion = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    Enlace = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Datos", x => x.Nombre);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Datos");
+        }
+    }
+}
